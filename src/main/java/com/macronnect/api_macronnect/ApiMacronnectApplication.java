090552a644b122757975e42b5308f4209e5c98f8
Eric Eduardo Amezcua Solano <ericamezcua3@gmail.com>
2026-07-02 15:34:06 -0700

    Inicialización de proyecto spring

---
+++ src/main/java/com/macronnect/api_macronnect/ApiMacronnectApplication.java
@@ -0,0 +1,13 @@
+package com.macronnect.api_macronnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMacronnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMacronnectApplication.class, args);
+	}
+
+}
